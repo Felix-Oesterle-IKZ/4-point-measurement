bb9da89fa70668976521b6da45f496b9c5ec1cf1
VFunke <vincent.funke@ikz-berlin.de>
2023-09-04 16:19:37 +0200

    Version 1.0

--- hauptprogramm.py
+++ hauptprogramm.py
@@ -25,8 +25,8 @@ def createFiles():
     with open(os.path.join(path, "data.csv"), "w", encoding="utf-8") as f: 
         f.write("Angeleger Strom I,Gemessende Spannung U,spezifischer Wiederstand rho,Gemessende Temperatur [°C],Messdaten\n")
         
-    with open(os.path.join(path, "mesurement_data"), "w", encoding="utf-8") as f: 
-        f.write(f"""Zieltemperaturen: {tTargetList}\nSpannungen: {IList}\n""")
+    with open(os.path.join(path, "measurement_data.csv"), "w", encoding="utf-8") as f: 
+        f.write(f"T_soll [°C],I [A],U_avg [V],U_std [V]\n")
         
     return path
 
@@ -42,6 +42,7 @@ def readRezept(debugPrint=True):
     tToleranz = [] # in °C
     tTime = []   # in min
     tStationaer = [] # in min
+    tStationaerTolerace = []
     
     for temp in rezept:
         line = temp.replace(" ", "")
@@ -58,45 +59,42 @@ def readRezept(debugPrint=True):
             tToleranz.append(line.split(",")[1])
             tTime.append(line.split(",")[2])
             tStationaer.append(line.split(",")[3])
+            tStationaerTolerace.append(line.split(",")[4])
     
     # remove the "r:"
     for i in range(len(tTarget)):
         tTarget[i] = tTarget[i].replace("r:", "")
         
     if debugPrint == True:
-        print(f"Tiefe: {tiefe}")
-        print(f"tTarget: {tTarget}")
-        print(f"tToleranz: {tToleranz}")
-        print(f"tTime:   {tTime}")
-        print(f"tStatio: {tStationaer}")
-        print(f"IList:   {IList}")
+        print(f"Tiefe:          {tiefe}")
+        print(f"tTarget:        {tTarget}")
+        print(f"tToleranz:      {tToleranz}")
+        print(f"tTime:          {tTime}")
+        print(f"tStatio:        {tStationaer}")
+        print(f"tStatioToleranz:{tStationaerTolerace}")
+        print(f"IList:          {IList}")
         print("-----------------------\n")
-    return tiefe, tTarget, tToleranz, tTime, tStationaer, IList
+        
+    return tiefe, tTarget, tToleranz, tTime, tStationaer, tStationaerTolerace, IList
 
 # Erg: Plotet mit den neusten Werten neu und legt die Grezen des Graphen neu fest.
-def plotData(ax, fig, tSensorList, vList, tSensorLine, vLine):
-    x1 = np.linspace(0,len(tSensorList),len(tSensorList))
-    x2 = np.linspace(0,len(uList),len(uList))
-    tSensorLine.set_xdata(x1)
-    uLine.set_xdata(x2)
-    ax.set_xlim([0, len(tSensorList)+1])
+def plotData(ax, fig, List, Line):
+    #x1 = np.linspace(0,len(tSensorList),len(tSensorList))
+    x2 = np.linspace(0,len(List),len(List))
+    #tSensorLine.set_xdata(x1)
+    Line.set_xdata(x2)
+    ax.set_xlim([0, len(List)+1])
     
-    if tSensorList != None:
-        tSensorLine.set_ydata(tSensorList) #plot new line
-        vLine.set_ydata(vList) #plot new line
-        ax.set_ylim([0, 30])
-        #limList = tSensorList
-        #limList.sort()
-        #limList = limList[1:]
-        #ax.set_ylim([ float(limList[0])-1, float(limList[-1])+1 ])
-    else:
-        ax.set_ylim([-.1, 100])
+    if List != None:
+        #tSensorLine.set_ydata(tSensorList) #plot new line
+        uLine.set_ydata(List) #plot new line
+        ax.set_ylim([0, 2])
         
     fig.canvas.draw()
     fig.canvas.flush_events()
 
 ### Prüfen ob die Temperatur Stationär ist
-def stationaerPruefung(temperaturListe, tStationaer, StationaerAbweichung=1):
+def stationaerPruefung(temperaturListe, tStationaer, tStationaerTolerace):
     isStationaer = False
     if len(temperaturListe) > tStationaer: # Nur prüfen wenn über StationarTime Einträge vohanden sind
         tempList = temperaturListe[int(-tStationaer*60):] # Liste der letzten Temperaturen erstellen...
@@ -105,7 +103,7 @@ def stationaerPruefung(temperaturListe, tStationaer, StationaerAbweichung=1):
         
         
         
-        if tempList[-1] - tempList[0] <= StationaerAbweichung: # Wenn die größte die Temperaur und die Kleinste Temp. kleiner sind als der vorggebene Bereich
+        if tempList[-1] - tempList[0] <= tStationaerTolerace: # Wenn die größte die Temperaur und die Kleinste Temp. kleiner sind als der vorggebene Bereich
             isStationaer = True
             #print("Stationaer")
         else:
@@ -113,14 +111,14 @@ def stationaerPruefung(temperaturListe, tStationaer, StationaerAbweichung=1):
             #print("Nicht Stationaer")
     return isStationaer
 
-#Sends Command to 2540 (slow!)
+#Sends Command to 2540 over LAN
 def sendCommand2450(command):
     driver.get("http://admin:admin@172.20.22.127/commands.html")
     input_text_cmd = driver.find_element(By.ID, 'cmd')
-    input_text_cmd.send_keys("*RCL 1")
-    time.sleep(2)
+    input_text_cmd.send_keys(command)
+    time.sleep(1.9)
     driver.find_element(By.ID, 'send').click()
-    driver.quit()
+    #driver.quit()
     
 # Ließt die Temperatur der Heizplatte, die Spannung der Probe und berechnet den Oberflächenwiedertand
 # Vor: Tiefe der Probe t in m
@@ -140,95 +138,84 @@ def getData(t, debugPrint=False):
             print(f"rho_2D={rho_2D}")
             print(f"{tSensor}°C", end="\n\n")
         return U, rho_2D, tSensor
-
-
-# Berechnet dden Durchschnitt einer Liste
-def avrgOfList(liste):
-    avrg = 0
-    for x in liste:
-        avrg = avrg + x
-    return avrg/len(liste)
+    
+    
+###########################################################################################
                                     ### ### ### BEGIN PREP ### ### ###
-#Selenium vorbereiten
-service = Service(executable_path='/usr/bin/chromedriver')
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
-
 # Load config data
 with open("config.yml", "r") as f:
     config = yaml.safe_load(f)
 
-daq = daq6510.Daq6510(config['DAQ-6510']) # Multimeter DAQ6510 vorbeiten
+# Selenium vorbereiten
+service = Service(executable_path='/usr/bin/chromedriver')
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 
-### Prepare Heizer
+# 2450 SorceMeter vorbereiten
+sendCommand2450(f"*RCL 0") #4-Spizzenmessung einstellen
+sendCommand2450(":OUTPut:STATe ON")
+
+# Multimeter DAQ6510 vorbeiten
+daq = daq6510.Daq6510(config['DAQ-6510'])
+
+
+
+# Prepare Heizer
 heizer.logging_on(True)
 heizer_config = config['Heizer']['Schnittstelle']
 sensor = heizer.HeizerPlatte(**heizer_config)
+sensor.start_heizung()
 
-
-### Listen
+# Listen, Variablen
 tSensorList = [0] # Stores all Temperatures from sensor internaly
 uList = [0] # Stores all Temperatures from Calibration instrument internaly
-
-
-tiefe, tTargetList, tToleranceList, tTimeList, tStationaerList, IList = readRezept() # tTargetList ist die Liste der Zieltempraturen,# tTimeList ist die Liste der "Verweilzeiten"
+tiefe, tTargetList, tToleranceList, tTimeList, tStationaerList, tStationaerToleraceList, IList = readRezept() # tTargetList ist die Liste der Zieltempraturen,# tTimeList ist die Liste der "Verweilzeiten"
 path = createFiles()
 
-
-### Plot Prep.
+# Plot Prep.
 x1 = 0
 x2 = 0
-fig, ax = plt.subplots(figsize=(10, 8))
-plt.title("Temperatur über Zeit", fontsize=20)
-plt.xlabel("Zeit [s]",fontsize=18)
-plt.ylabel("Temperatur [°C]",fontsize=18)
+fig, ax = plt.subplots(figsize=(8, 6))
+plt.title("Spannung über Zeit", fontsize=16)
+plt.xlabel("Zeit [s]",fontsize=14)
+plt.ylabel("Spannung [V]",fontsize=14)
+ax.set_ylim([0, 1])
 plt.grid()
 plt.ion()
 plt.show()
 tSensorLine, = plt.plot(x1, tSensorList)
 uLine, = plt.plot(x2, uList)
 
-
+###########################################################################################
                          ### ### ### BEGIN LOOP ### ### ###
-firstTime = True
-#k = 1 # korrekturfaktor oE
-#s = 0.03 #Strecke zwischen zwei Messspitzen in m
+
 for i in range(len(tTargetList)): # Für jede Temperatur:
     
     tTarget     = float(tTargetList[i])
     tTolerance  = float(tToleranceList[i])
     tTime       = float(tTimeList[i])
     tStationaer = float(tStationaerList[i])
+    tStationaerTolerace = float(tStationaerToleraceList[i])
     data_points = 0
     sensor.change_SollTemp(str(tTarget))
     print(f"Nächste Temperatur!\ntTarget={tTarget}°C")
-    
-    with open(os.path.join(path, "mesurement_data"), "a", encoding="utf-8") as f:
-        f.write(f"\nZieltemperatur: {tTarget}°C\n")
         
     for tempI in IList: #Für jeden Strom in jeder Temperatur:
-        rho_2DList = []
+        uListTemp = []
+        data_points = 0
         
-        sendCommand2450(f"*RCL {tempI}")
+        I = float(tempI)
+        sendCommand2450(f"SOURce:CURRent {I}")
         
-        if int(tempI) == 0:
-            I = 0.150
-        elif int(tempI) == 2:
-            I = 0.450
-        elif int(tempI) == 4:
-            I = 0.750
-        else:
-            print(f"ERROR: tempI={tempI}")
-            
-        print(f"Nächster Strom!\nI={I}A")
+        print(f"Nächster Strom!\n  I={I}A")
         while True:
-            U, rho_2D, tSensor = getData(float(tiefe)/1000, True) # Get Data
+            U, rho_2D, tSensor = getData(float(tiefe)/1000, False) # Get Data
         
             ### Save data internaly for plot Data function
             tSensorList.append(tSensor)
             uList.append(U)
         
-            isStationaer = stationaerPruefung(tSensorList, tStationaer)
+            isStationaer = stationaerPruefung(tSensorList, tStationaer, tStationaerTolerace)
         
             ### Save data externaly
             line = f"{I},{U},{rho_2D},{tSensor},{isStationaer}\n"
@@ -237,32 +224,25 @@ for i in range(len(tTargetList)): # Für jede Temperatur:
         
             ### Check if tSensor is in Target Area and stationary
             if tSensor <= (tTarget + tTolerance) and tSensor >= (tTarget - tTolerance) and isStationaer == True:
-            
-                if firstTime == True: #execute only first time in Loop
-                    firstTime = False
-                    data_points = 0
-                    #beginMesurement(tTarget, I)
-                    
-                rho_2DList.append(rho_2D)
-                """
-                line = f"    {rho_2D}\n"
-                with open(os.path.join(path,"mesurement_data"), "a") as f:
-                    f.write(line)
-                """
+                uListTemp.append(U)
                 data_points = data_points+1
             
                 # Skip to next Step in Sequence
                 if data_points >= tTime*60:
                     firstTime = True
-                    with open(os.path.join(path,"mesurement_data"), "a", encoding="utf-8") as f:
-                        line = f"  Oberflächenwiederstand bei {I}A: {round(avrgOfList(rho_2DList),3)}\n"
+                    with open(os.path.join(path,"measurement_data.csv"), "a", encoding="utf-8") as f:
+                        line = f"{tTarget},{I},{round(np.mean(uListTemp),4)},{round(np.std(uListTemp),4)}\n"
                         f.write(line)
+                    uListTemp = []
                     break
         
-        
-            plotData(ax, fig, tSensorList, uList, tSensorLine, uLine)
-        
+            plotData(ax, fig, uList, uLine)
+            #plotData(ax, fig, tSensorList, tSensorLine)
+            
             time.sleep(1)
 
 # Programm beenden:
 sensor.stop_heizung()
+plt.savefig(os.path.join(path,"plot.png"))
+plt.close()
+driver.quit()
